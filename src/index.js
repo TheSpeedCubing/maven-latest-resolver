f36152353304780f8bb00426ecc6bdd7a28e85b2
speedcubing <speedcubing@speedcubing.top>
2025-10-05 14:29:06 +0800

    feat: let {artifactid}-{version}.jar works.
    example:
    cubingpaper-1.8-R0.1-SNAPSHOT.jar
    you don't need to use cubingpaper-latest.jar

--- src/index.js
+++ src/index.js
@@ -6,48 +6,54 @@ const app = express();
 
 const REPO_URL_BASE = process.env.REPO_URL_BASE || 'https://repo.maven.apache.org/maven2';
 const PORT = process.env.PORT || 80;
-const REDIRECT_URL_REMOVE = process.env.REDIRECT_URL_REMOVE || '';
+
 
 app.get('*path', async (req, res) => {
-  let path = req.path
-  if (path.startsWith(REDIRECT_URL_REMOVE)) {
-    path = path.slice(REDIRECT_URL_REMOVE.length);
-  }
-  const pathArr = path.split("/")
-  
-  const regex = /-latest\.[\w.]+$/;
-  if (!regex.test(pathArr[pathArr.length -1])) {
-    res.redirect(REPO_URL_BASE + path);
-    return;
-  }
 
   try {
+    // /{groupId}/{artifactID}/{version}/{artifactID}-{version}.jar
+    const pathArr = req.path.split("/")
+
+    // {version}
+    const version = pathArr[pathArr.length - 2];
+
+    // check if {version} ends with -SNAPSHOT
+    if (!version.toUpperCase().endsWith("-SNAPSHOT")) {
+      res.sendStatus(404);
+      return;
+    }
     
     const headers = {};
     if (req.headers.authorization) {
       headers.Authorization = req.headers.authorization;
     }
-  
+
+    // /{groupId}/{artifactID}/{version}/maven-metadata.xml
     const paths = [...pathArr];
     paths.pop();
     paths.push('maven-metadata.xml');
-    
+
+    // {REPO_URL_BASE} /{groupId}/{artifactID}/{version}/maven-metadata.xml
     const metadataURL = REPO_URL_BASE + paths.join("/");
     
     const response = await axios.get(metadataURL, {headers});
     const metadata = await xml2js.parseStringPromise(response.data);
-    
+
+    // <snapshotVersions> array
     const snapshotVersions = metadata?.metadata?.versioning?.[0]?.snapshotVersions?.[0]?.snapshotVersion;
-    const snapshotVersion = snapshotVersions?.[1]?.value?.[0];
+
+    // get latest <snapshotVersion>
+    const jarSnapshot = snapshotVersions?.find(v => v.extension?.[0] === 'jar');
+    const snapshotVersion = jarSnapshot?.value?.[0];
+
     if (!snapshotVersion) {
       throw new Error('Snapshot version not found in metadata.');
     }
 
     paths.pop();
-    
-    const finalURL = REPO_URL_BASE + paths.join('/') + "/" + pathArr[pathArr.length - 1].replace("latest", snapshotVersion)
 
-    console.log(req.url + " -> " + finalURL)
+    // {REPO_URL_BASE} /{groupId}/{artifactID}/{version} / {artifactID}-{version}.jar -> replace 
+    const finalURL = REPO_URL_BASE + paths.join('/') + "/" + pathArr[pathArr.length - 1].replace(version, snapshotVersion)
     const fileResponse = await axios.get(finalURL, {
       headers, 
       responseType: 'stream'
@@ -59,10 +65,11 @@ app.get('*path', async (req, res) => {
     }
     fileResponse.data.pipe(res);
 
+    return;
   } catch (err) {
     console.error(err);
-    res.status(500).send('Failed to resolve latest jar');
   }
+  res.sendStatus(404);
 });
 
 app.listen(PORT, () => {
